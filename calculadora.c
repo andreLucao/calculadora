061ccd2e8664c13b93e5e8890eec7a6439b6797a
Luca <luccadeandrade11@gmail.com>
2024-05-13 19:33:01 -0300

    version1

---
+++ calculadora.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int potencia(int base, int expoente)
+{
+  int resultado = 1;
+  for (int i = 0; i < expoente; i++)
+  {
+    resultado = base; 
+  }
+  return resultado;
+
+  //adicionar para nmrs float
+}
+
+long fatorial(int fator)
+{
+  long valor = 1;
+  for (int i = 0; i < fator - 1; i++)
+  {
+    valor= (fator - i);
+  }
+  return valor;
+}
+
+
+
+int main()
+{
+  int resultado = fatorial(5);
+  printf("%d", resultado);
+
+  int variable;
+
+}
